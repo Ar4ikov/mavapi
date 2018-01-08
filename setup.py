602f916096364963a95884575329fe5f1c15ca56
Nikita <bizy18588@gmail.com>
2018-01-08 14:24:02 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mavapi',
+    version='1.0.1',
+    packages=['mavapi'],
+    url='vk.com/ar4ikov',
+    license='Apache 2.0',
+    author='Nikita Archikov',
+    author_email='bizy18588@gmail.com',
+    description='A simple MAV API wrapper for python (3.5 or newer)'
+)
