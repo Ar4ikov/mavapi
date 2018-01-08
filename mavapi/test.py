546896b2ccaea631649453fdd4967136d94dbd45
Nikita <bizy18588@gmail.com>
2018-01-08 14:46:59 +0300

    Delete test.py

--- mavapi/test.py
+++
@@ -1,5 +0,0 @@
-import requests
-data = {'access_token': '26ueJCO8-SkAw-KZj2-nrzJ-0E4vhOW9O8Pa', 'mav_id': '16'}
-a = requests.post('https://mav-server.ru/api?getUser', data=data)
-a = a.text
-print(a)
