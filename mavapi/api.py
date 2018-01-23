041efeb3b8a0ed19ba50537d60e1e392d7349b05
Nikita <bizy18588@gmail.com>
2018-01-23 22:16:22 +0300

    Update api.py

--- mavapi/api.py
+++ mavapi/api.py
@@ -1,31 +1,38 @@
 from mavapi.exceptions import MAVAPIError,MAVAPIAuthError
-import requests
+import aiohttp
+import asyncio
 import re
 
-__version__ = '2.0.0'
+__version__ = '2.1.3'
 
 class API:
     def __init__(self,server=None):
         self.server = server
 
     # Делаем все запросы вот этой штукой, фактически сердце модуля
-    def getRequest(self,server=None,params=None, data=None):
-        request = requests.post(server, params=params, data=data).text
+    '''
+        Обратите внимание! Все запросы идут корутинами, и их можно получить через event_loop
+    '''
+    async def getRequest(self,server=None,params=None, data=None):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(server, params=params, data=data) as request:
 
-        request = re.sub('true','1',request)
-        request = re.sub('false', '0', request)
-        request = re.sub('null', 'None', request)
-        request = eval(request)
+                request = await request.text()
+                request = re.sub('true','1',request)
+                request = re.sub('false', '0', request)
+                request = re.sub('null', 'None', request)
+                request = eval(request)
 
-        return request['content']
-
+                return request['content']
     '''
         Авторизуемся здесь
         По факту, тут нет авторизации, просто проверяем валидность токена, делая запрос на сервер и обрабатываем запрос
         Если токен неверен - поднимаем ошибку и не даём скрипту выполняться дальше
     '''
     def Auth(self,access_token=None):
-        request = self.getRequest(self.server, params='getTokenPerms', data={'access_token': access_token})
+        # Вызов event loop'а
+        loop = asyncio.new_event_loop()
+        request = loop.run_until_complete(self.getRequest(self.server, params='getTokenPerms', data={'access_token': access_token}))
         if request['status'] == 'error':
             if request['error_code'] in MAVAPIError.error_list.keys():
                 raise MAVAPIAuthError(MAVAPIError.error_list[request['error_code']])
@@ -49,7 +56,8 @@ class API:
 
     def getResponse(self,method=None,**kwargs):
         data = self.getData(kwargs)
-        request = self.getRequest(self.server,params=method,data=data)
+        loop = asyncio.new_event_loop()
+        request = loop.run_until_complete(self.getRequest(self.server,params=method,data=data))
         if request['status'] == 'error':
             raise MAVAPIError(request)
         else:
